6f5ef2b642d4f3a85e821f24e9110b4327cb05dd
Ricky Hougland <houglande@gmail.com>
2016-12-05 22:38:18 -0800

    Day 5, part 2 finished

--- day1/src/com/hougland/adventofcode/day5/Main.java
+++ day1/src/com/hougland/adventofcode/day5/Main.java
@@ -12,6 +12,50 @@ public class Main {
         final String realInput = "ojvtpuvg";
 //        final String testInput = "abc";
 
+        // Part 2
+        final String[] solution = new String[8];
+        int solutionLength = 0;
+
+        try {
+            MessageDigest m = MessageDigest.getInstance("MD5");
+            m.reset();
+
+            int hexCounter = 0;
+            while (solutionLength < 8) {
+                String hex = "00001";
+                while (!"00000".equals(hex.substring(0, 5))) {
+                    byte[] toHash = (realInput + String.valueOf(hexCounter)).getBytes();
+                    hex = (new HexBinaryAdapter()).marshal(m.digest(toHash));
+                    hexCounter++;
+                }
+
+                String indexString = hex.substring(5, 6);
+                String value = hex.substring(6, 7);
+                if (indexString.matches("[0-7]")) {
+                    int index = Integer.parseInt(indexString);
+
+                    if (solution[index] == null) {
+                        solution[index] = value;
+                        solutionLength++;
+                    }
+                }
+            }
+
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println(e);
+        }
+
+        // convert solution array into string
+        StringBuilder builder = new StringBuilder();
+        for (String string : solution) {
+            builder.append(string);
+        }
+        String solutionString = builder.toString();
+
+        System.out.println(solutionString);
+    }
+
+    public static void part1(String input) {
         final List<String> five0s = new ArrayList<>();
 
         try {
@@ -22,7 +66,7 @@ public class Main {
             for (int i = 0; i < 8; i++) {
                 String hex = "00001";
                 while (!"00000".equals(hex.substring(0, 5))) {
-                    byte[] toHash = (realInput + String.valueOf(hexCounter)).getBytes();
+                    byte[] toHash = (input + String.valueOf(hexCounter)).getBytes();
                     hex = (new HexBinaryAdapter()).marshal(m.digest(toHash));
                     hexCounter++;
                 }
