6e5c9963ac734d90a907614fd9184d2b27f30175
Ricky Hougland <houglande@gmail.com>
2016-12-28 20:11:10 -0600

    Possibly finished day 16 part 1 - on an airplane and can't check it yet

--- day1/src/com/hougland/adventofcode/day16/Main.java
+++ day1/src/com/hougland/adventofcode/day16/Main.java
@@ -1,9 +1,7 @@
 package com.hougland.adventofcode.day16;
 
 //You're done scanning this part of the network, but you've left traces of your presence. You need to overwrite some disks with random-looking data to cover your tracks and update the local security system with a new checksum for those disks.
-//
 //For the data to not be suspicious, it needs to have certain properties; purely random data will be detected as tampering. To generate appropriate random data, you'll need to use a modified dragon curve.
-//
 //Start with an appropriate initial state (your puzzle input). Then, so long as you don't have enough data yet to fill the disk, repeat the following steps:
 //
 //Call the data you have at this point "a".
@@ -45,5 +43,89 @@ package com.hougland.adventofcode.day16;
 //The first disk you have to fill has length 272. Using the initial state in your puzzle input, what is the correct checksum?
 //
 //Your puzzle input is 10011111011011001.
+
 public class Main {
+
+    // possible answer: 10111110010110110
+
+    public static void main(String[] args) {
+        final int diskSpace = 272;
+        final String input = "10011111011011001";
+//        final int diskSpace = 20;
+//        final String input = "10000";
+
+        String data = getDataForDiskSpace(input, diskSpace);
+        String checksum = getOddChecksum(data);
+
+        System.out.println("Checksum: " + checksum);
+    }
+
+    public static String getOddChecksum(final String data) {
+        String checksum = data;
+
+        while (checksum.length() % 2 == 0) {
+            checksum = getChecksum(checksum);
+        }
+
+        return checksum;
+    }
+
+    public static String getChecksum(final String data) {
+        String[] chars = data.split("");
+        String checksum = "";
+
+        for (int i = 0; i < chars.length; i += 2) {
+            if (chars[i + 1].equals(chars[i])) {
+                checksum += "1";
+            } else {
+                checksum += "0";
+            }
+        }
+
+        return checksum;
+    }
+
+    public static String getDataForDiskSpace(final String input, final int diskSpace) {
+        String data = input;
+
+        while (data.length() < diskSpace) {
+            data = getData(data);
+        }
+
+        return data.substring(0, diskSpace);
+    }
+
+    public static String getData(final String input) {
+        String a = input;
+        String b = reverseString(a);
+        b = replaceChars(b);
+
+        return a + "0" + b;
+    }
+
+    public static String replaceChars(final String input) {
+        String[] chars = input.split("");
+        String replaced = "";
+
+        for (int i = 0; i < chars.length; i++) {
+            if ("1".equals(chars[i])) {
+                replaced += "0";
+            } else {
+                replaced += "1";
+            }
+        }
+
+        return replaced;
+    }
+
+    public static String reverseString(final String input) {
+        String[] chars = input.split("");
+        String reversed = "";
+
+        for (int i = chars.length - 1; i >= 0; i--) {
+            reversed += chars[i];
+        }
+
+        return reversed;
+    }
 }
